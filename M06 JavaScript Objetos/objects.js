82caead661242d2c927f13fd16c11276aa6a627c
Gr4u <joseluisgrauc@gmail.com>
2023-09-01 16:57:47 -0300

    Modulo 06 completado

---
+++ M06 JavaScript Objetos/objects.js
@@ -0,0 +1,22 @@
+//var deportes = {
+//    conBalon: ['Futbol', 'Basketball', 'Golf'],
+//    sinBalon: ['Boxeo', 'Surf', 'Trekking'],
+// };
+var persona = { nombre: 'Lucas', edad: 26, estudios: { esProgramador: true } };
+
+// console.log(persona.edad);//
+persona.nombre = "Martin";
+// console.log(persona.nombre);//
+
+persona.edad = 32;
+
+// console.log(persona.edad);
+
+var autos = {};
+autos.marcas = ['Ford', 'Audi', 'Ferrari'];
+//console.log(autos);
+
+//CREAR
+var autos = {};
+autos.marcas = ['Ford', 'Audi', 'Ferrari'];
+console.log(autos);
