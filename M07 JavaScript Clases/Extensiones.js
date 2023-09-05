14c345ab9e19fe7a22eeea0ee304d9ecd7d79f9d
Gr4u <joseluisgrauc@gmail.com>
2023-09-05 16:43:45 -0300

    Modulo 07 completado

---
+++ M07 JavaScript Clases/Extensiones.js
@@ -0,0 +1,32 @@
+class Programador extends Persona {
+    constclass Persona {
+        constructor(nombre, edad) {
+           this.nombre = nombre;
+           this.edad = edad;
+        }
+        saludar() {
+           console.log(
+              'Hola, mi nombre es  ' + this.nombre + '. Tengo  ' + this.edad
+           );
+        }
+     }
+     let martin = new Persona('Martin', 26);
+     martin.saludar();ructor(nombre, edad, añosDeExperiencia) {
+       super(nombre, edad);
+       this.añosDeExperiencia = añosDeExperiencia;
+    }
+    codear() {
+       console.log(
+          'Soy ' +
+             this.nombre +
+             ' . Codeo desde hace  ' +
+             this.añosDeExperiencia +
+             ' años'
+       );
+    }
+ }
+ 
+ let martin = new Persona('Martín', 26);
+ let programador = new Programador('María', 37, 4);
+ martin.saludar();
+ programador.codear();
